a63fa057ee45c4eeb2a00e229a71261203b96844
PreyMa <mlpreymann@gmail.com>
2022-04-25 15:53:40 +0200

    TOC: Render sub headings as nested lists

--- extensions/tableOfContents.js
+++ extensions/tableOfContents.js
@@ -8,19 +8,34 @@ class TableOfContents extends Kek.Token.CustomBlock {
     this.table= null;
   }
 
+  resourceType() {
+    return 'none';
+  }
+
   setTable( t ) {
     this.table= t;
   }
 
-  render() {
-    const children= this.table.map( tk => {
+  _renderLevel( table ) {
+    const children= table.map( tk => {
+      // Render nested level
+      if( Array.isArray(tk) ) {
+        return this._renderLevel( tk );
+      }
+
+      // Create header link as list element
       const linkElem= new Kek.HtmlElementBuilder('a', new Kek.HtmlTextBuilder(tk.title()));
       linkElem.setAttribute('href', `#${tk.fragmentId()}`);
+
       return new Kek.HtmlElementBuilder('li', linkElem);
     });
 
-    return  new Kek.HtmlElementBuilder('p',
-              new Kek.HtmlElementBuilder('ol', ...children ));
+    // Create ordered list filled with children
+    return new Kek.HtmlElementBuilder('ol', ...children );
+  }
+
+  render() {
+    return  new Kek.HtmlElementBuilder('p', this._renderLevel( this.table ));
   }
 }
 
@@ -41,9 +56,36 @@ export class TableOfContentsExtension extends Kek.Extension {
   }
 
   preRender( kek ) {
+    let currentLevel= 1;
+    let currentArray= this.table;
+
     kek.document.forEachOfType(FragmentToken, tk => {
+      // Only add items with title
       if( tk.title() ) {
-        this.table.push( tk );
+        // Header might change current level
+        if( tk instanceof Kek.Token.Header ) {
+          // Add new nested levels
+          const level= tk.headerLevel();
+          while( level > currentLevel ) {
+            const arr= [];
+            currentArray.push(arr);
+            currentArray= arr;
+            currentLevel++;
+          }
+
+          // Move down levels
+          if( level < currentLevel ) {
+            currentArray= this.table;
+            for( let i= 1; i< level; i++ ) {
+              currentArray= currentArray[currentArray.length-1];
+              Kek.assert( Array.isArray(currentArray) );
+            }
+
+            currentLevel= level;
+          }
+        }
+
+        currentArray.push( tk );
       }
     });
 
