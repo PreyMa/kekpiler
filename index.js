2846564ad9b73377d15e72db5778bbb7f9be0b51
PreyMa <mlpreymann@gmail.com>
2022-07-15 23:07:28 +0200

    Add temml math extension to default compiler config
    The extension only gets added if temml is detected and can be loaded.

--- index.js
+++ index.js
@@ -2,18 +2,37 @@ import * as Kek from './kekpiler.js';
 import {TableOfContentsExtension} from './extensions/tableOfContents.js';
 import {HeaderSluggerExtension} from './extensions/headerSlugger.js';
 import {LineBreakExtension} from './extensions/lineBreak.js';
+import {TemmlMathExtension} from './extensions/temmlMath.js';
 
-function getCompiler() {
+let Temml= null;
+let temmlLoadFailed= false;
+
+async function getCompiler() {
+  // Try to load temml lib once
+  if( !Temml && !temmlLoadFailed ) {
+    try {
+      Temml= (await import('./dependency/temml.cjs')).default;
+    } catch( e ) {
+      temmlLoadFailed= true;
+      console.error( e );
+      throw e;
+    }
+  }
+
   const k= new Kek.Kekpiler();
   k.use( new TableOfContentsExtension() );
   k.use( new HeaderSluggerExtension() );
   k.use( new LineBreakExtension() );
 
+  if( Temml ) {
+    k.use( new TemmlMathExtension(Temml) )
+  }
+
   return k;
 }
 
 async function compileMarkdown( markdownText ) {
-  const comp= getCompiler();
+  const comp= await getCompiler();
   const html= await comp.compile( markdownText, true );
 
   comp.printMessages( Kek.ConsolePrinter.the() );
